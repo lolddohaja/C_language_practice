d0a932f78ba64fb233e17e4c8de8f67c9b0c307f
lolddohaja <sksmsrlqja05@gmail.com>
2021-04-21 14:55:05 +0900

    미완

--- Day2/homework_1.c
+++ Day2/homework_1.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 #define WIDTH 7
 #define HEIGHT 6
 
-void menu(void);
-void printScreen(char *arr_p, int width, int height);
-void flipH(char *src, char *dest, int width, int height);
+void printScreen(char arr[][WIDTH], int height);
+void printScreen2(char arr[][HEIGHT], int width);
+void flipH(char src[][WIDTH], char dest[][WIDTH], int height);
 void flipV(char src[][WIDTH], char dest[][WIDTH], int height);
 void rotate9(char src[][WIDTH], char dest[][HEIGHT], int width);
 void rotate18(char src[][WIDTH], char dest[][WIDTH], int height);
@@ -15,8 +14,7 @@ void invertf(char src[][WIDTH], char dest[][WIDTH], int height);
 int main(void)
 {
     int  n;
-    //문자열 어레이로 표현 가능 
-    //구조체로 만들어서 표현 가능
+
     char origin[HEIGHT][WIDTH] = {
         {'-','-','-','*','-','-','-'},
         {'-','-','-','*','-','-','-'},
@@ -32,54 +30,62 @@ int main(void)
     char rotate_270[WIDTH][HEIGHT];
     char invert[HEIGHT][WIDTH];
 
+
+
     while(1){
-        menu();
-        printf("선택하세요:");
+        printf("1.플립H, 2.플립V, 3.90도돌려, 4.180도돌려, 5.270도 돌려, 6.인버트 (0.종료): ");
         scanf("%d",&n);
         if(n==0) break;
+
         //print origin
         printf("\nOriginal \n");
-        printScreen((char *)origin, WIDTH, HEIGHT);
+        printScreen(origin, HEIGHT);
+
+
         switch (n)
         {
         //flipH
-        case 1:
-            flipH(origin, flip_h, WIDTH,HEIGHT);
-            printScreen((char *)flip_h, WIDTH, HEIGHT);
-            printf("\nflip_h \n");
-            break;
+        case 1:{
+           
+            flipH(origin, flip_h, HEIGHT);
+            printScreen(flip_h, HEIGHT);
+        }
+        break;
         //flipV
-        case 2:
+        case 2:{
             flipV(origin, flip_v, HEIGHT);
-            printScreen((char *)flip_v, WIDTH, HEIGHT);
-            printf("\nflip_v \n");
-            break;
+            printScreen(flip_v, HEIGHT);
+     
+        }
+        break;
         //90도 돌리기
-        case 3:
+        case 3:{
             rotate9(origin, rotate_90, HEIGHT);
-            printScreen((char *)rotate_90, HEIGHT, WIDTH);
-            printf("\n90도 돌리기 \n");
-            break;
+            printScreen2(rotate_90, WIDTH);
+        }
+        break;
         //180도 돌리기
-        case 4:
+        case 4:{
             rotate18(origin, rotate_180, HEIGHT);
-            printScreen((char *)rotate_180, WIDTH, HEIGHT);
-            printf("\nrotate 180\n");
-            break;
+            printScreen(rotate_180, HEIGHT);
+        }
+        break;
         //270도 돌리기
-        case 5:
+        case 5:{
             rotate27(origin, rotate_270, HEIGHT);
-            printScreen((char *)rotate_270, HEIGHT, WIDTH);
-            printf("\nrotate 270\n");
-            break;
+            printScreen2(rotate_270, HEIGHT);
+        }
+        break;
         //인버트
-        case 6:
+        case 6:{
             invertf(origin, invert, HEIGHT);
-            printScreen((char *)invert, WIDTH, HEIGHT);
-            printf("\n인버트 \n");
-            break;
+            printScreen(invert, HEIGHT);
+        }
+        break;
         default: break;
         }
+
+
     }
     // move x, y
     // zoom
@@ -87,29 +93,31 @@ int main(void)
     return 0;
 }
 
-void menu(void){
-    printf("-------골라보세요-------");
-    printf("       1번 플립H        ");
-    printf("       2번 플립V        ");
-    printf("      3번 90도돌려      ");
-    printf("      4번 180도돌려     ");
-    printf("      5번 270도돌려     ");
-    printf("       6번 인버트       ");
-    printf("       (0번 종료)       ");
-}
-
-void printScreen(char *arr_p, int width, int height){
+void printScreen(char arr[][WIDTH], int height){
     for(int y = 0; y < height; y++)
     {
-        for(int x = 0; x < width; x++)
+        for(int x = 0; x < WIDTH; x++)
         {
-            printf("%c", *(arr_p + (y*width) + x) );
+            printf("%c", arr[y][x]);
         }
         printf("\n");
     }
 }
 
-void flipH(char *src, char *dest, int width, int height){
+void printScreen2(char arr[][HEIGHT], int width){
+    for(int i = 0; i < WIDTH; i++)
+    {
+        for(int j = 0; j < HEIGHT; j++)
+        {
+            printf("%c", arr[i][j]);
+        }
+
+
+        printf("\n");
+    }
+}
+
+void flipH(char src[][WIDTH], char dest[][WIDTH], int height){
     for (int y = 0; y < HEIGHT; y++)
     {
         for(int x = 0; x < WIDTH; x++)
@@ -117,7 +125,7 @@ void flipH(char *src, char *dest, int width, int height){
             dest[y][x] = src[y][(WIDTH-1)-x];
         }
     }
-    
+    printf("\nflip_h \n");
 }
 
 void flipV(char src[][WIDTH], char dest[][WIDTH], int height){
@@ -128,20 +136,21 @@ void flipV(char src[][WIDTH], char dest[][WIDTH], int height){
             dest[y][x] = src[(HEIGHT-1)-y][x];
         }
     }
-    
+    printf("\nflip_V \n");
 }
 
 void rotate9(char src[][WIDTH], char dest[][HEIGHT], int width){
 
     for (int i = 0; i < HEIGHT; i++)
     {
-        for(int j = 0 ; j < WIDTH ; j++)
+        for(int j = 0 ; j < WIDTH-(WIDTH-HEIGHT); j++)
         {
             dest[j][HEIGHT-1-i] = src[i][j];
         }
     }
-    
+    printf("\n90도 돌리기 \n");
 }
+
 
 void rotate18(char src[][WIDTH], char dest[][WIDTH], int height){
     for (int i = 0; i < HEIGHT; i++)
@@ -151,18 +160,18 @@ void rotate18(char src[][WIDTH], char dest[][WIDTH], int height){
             dest[i][j] = src[HEIGHT-1-i][WIDTH-1-j];
         }
     }
-    
+    printf("\nrotate 180\n");
 }
 
 void rotate27(char src[][WIDTH], char dest[][HEIGHT], int width){
     for (int i = 0; i < HEIGHT; i++)
     {
-        for(int j = 0; j < WIDTH; j++)
+        for(int j = 0; j < WIDTH-(WIDTH-HEIGHT); j++)
         {
             dest[WIDTH-1-j][i] = src[i][j];
         }
     }
-    
+    printf("\nrotate 270\n");
 }
 
 void invertf(char src[][WIDTH], char dest[][WIDTH], int height){
@@ -178,4 +187,5 @@ void invertf(char src[][WIDTH], char dest[][WIDTH], int height){
             }
         }
     }
+    printf("\n인버트 \n");
 }
