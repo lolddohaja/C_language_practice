83a7fbac3b548b8339ae4c4e6377f1bc3db73dac
jiseok_kim <kimjisuk145@gmail.com>
2021-04-23 10:57:37 +0900

    asdasd

--- Day4/game.c
+++ Day4/game.c
@@ -5,17 +5,19 @@
 #include <termios.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <locale.h>
 
 #define SIZE 1024
+
 #define WALL '#'
 #define GOAL 'G'
 #define PLAYER '@'
 #define EMPTY ' '
 
-#define WALL_PAIR     1
-#define GOAL_PAIR     2
-#define PLAYER_PAIR   3
-#define EMPTY_PAIR    4
+#define WALL_PAIR 1
+#define GOAL_PAIR 2
+#define PLAYER_PAIR 3
+#define EMPTY_PAIR 4
 
 struct Map
 {
@@ -89,25 +91,31 @@ void loadMap(struct Map *selectMap, char *mapName)
 void printMap(struct Map map, int cnt)
 {
     refresh();
-    
     for (int y = 0; y < map.height; y++)
     {
         for (int x = 0; x < map.width; x++)
         {
-            if (*(map.map + (y * map.width) + x)=='#')
+            if (*(map.map + (y * map.width) + x) == '#')
             {
                 attron(COLOR_PAIR(WALL_PAIR));
             }
-            else if (*(map.map + (y * map.width) + x)=!'#')
+            else
             {
-                attroff(COLOR_PAIR(WALL_PAIR));
+                if (*(map.map + (y * map.width) + x) == 'G')
+                {
+                    attron(COLOR_PAIR(GOAL_PAIR));
+                }
+                else
+                {
+                    attroff(COLOR_PAIR(GOAL_PAIR));
+                    attroff(COLOR_PAIR(WALL_PAIR));
+                }
             }
+
             mvprintw(y, x, "%c", *(map.map + (y * map.width) + x));
-            
         }
     }
     attron(COLOR_PAIR(EMPTY_PAIR));
-    mvprintw(map.height, 0, "cnt: %d", cnt);
     attroff(COLOR_PAIR(EMPTY_PAIR));
     refresh();
 }
@@ -137,44 +145,37 @@ int get_dy(char ch)
     }
     return 0;
 }
-int map_menu()
+void map_menu()
 {
-    printf("--------Select Map---------");
-    printf("----------level1-----------");
-    printf("----------level2-----------");
-    printf("----------level3-----------");
-    printf("----------level4-----------");
-    printf("----------level5-----------");
-    printf("----------level6-----------");
-    printf("----------level7-----------");
+
+    refresh();
+    mvprintw(0, 0, "----------Select Map----------");
+    mvprintw(1, 0, "----------1. level1-----------");
+    mvprintw(2, 0, "----------2. level2-----------");
+    mvprintw(3, 0, "----------3. level3-----------");
+    mvprintw(4, 0, "----------4. level4-----------");
+    mvprintw(5, 0, "----------5. level5-----------");
+    mvprintw(6, 0, "----------6. level6-----------");
+
+    refresh();
 }
 
-
-int main(void)
+void start_game(struct Map current_map)
 {
-    initscr();
-    start_color();
-    init_pair(WALL_PAIR, COLOR_YELLOW, COLOR_RED);
-    init_pair(GOAL_PAIR, COLOR_MAGENTA, COLOR_GREEN);
-    init_pair(PLAYER_PAIR, COLOR_BLACK, COLOR_WHITE);
-    init_pair(EMPTY_PAIR, COLOR_WHITE, COLOR_BLACK);
-
-    struct Map first_map;
-    loadMap(&first_map, "map1.txt");
-
     int count = 0;
-    int x1 = 1, y1 = first_map.height-2;
-    int x2 = 40, y2 = 10;
+    int x1 = 1, y1 = current_map.height - 2;
+    char ch = 0;
     do
     {
+
         attron(COLOR_PAIR(PLAYER_PAIR));
-        mvaddch(y1,x1, PLAYER);
+        mvaddch(y1, x1, PLAYER);
         attroff(COLOR_PAIR(PLAYER_PAIR));
-        
-        *(first_map.map + (y1 * first_map.width) + x1) = '@';
-        
-        printMap(first_map, count);
-        char ch = 0, ch_buf;
+
+        *(current_map.map + (y1 * current_map.width) + x1) = '@';
+
+        printMap(current_map, count);
+
         while (kbhit())
         {
             ch = getchar();
@@ -183,29 +184,122 @@ int main(void)
         {
             attron(COLOR_PAIR(EMPTY_PAIR));
             mvaddch(y1, x1, EMPTY);
-            *(first_map.map + (y1 * first_map.width) + x1) = ' ';
+            *(current_map.map + (y1 * current_map.width) + x1) = ' ';
             attroff(COLOR_PAIR(EMPTY_PAIR));
-            
-            if (ch == 'q') break;
+
+            if (ch == 'q'){
+                clear();
+                break;
+            }
+
 
             int next_x = x1 + get_dx(ch);
             int next_y = y1 + get_dy(ch);
-            
-            if(next_x > 0 && next_x < first_map.width -1 && (*(first_map.map + (next_y * first_map.width) + next_x)) != '#')
+
+            mvprintw(current_map.height, 10, "current: %d", *(current_map.map + ((next_y - 1) * current_map.width) + next_x));
+
+            if (*(current_map.map + ((next_y + 1) * current_map.width) + next_x) == ' ' && (ch == 'a' || ch == 'd'))
+            {
+                ch = 0;
+            }
+            if (*(current_map.map + ((next_y - 1) * current_map.width) + next_x) == ' ' && (ch == 'a' || ch == 'd'))
+            {
+                ch = 0;
+            }
+            if (*(current_map.map + ((next_y)*current_map.width) + next_x + 1) == ' ' && (ch == 'w' || ch == 's'))
+            {
+                ch = 0;
+            }
+            if (*(current_map.map + ((next_y)*current_map.width) + next_x - 1) == ' ' && (ch == 'w' || ch == 's'))
+            {
+                ch = 0;
+            }
+
+            if (next_x > 0 && next_x < current_map.width - 1 && (*(current_map.map + (next_y * current_map.width) + next_x)) != '#')
             {
                 x1 = next_x;
             }
-            if(next_y > 0 && next_y < first_map.height -1 && (*(first_map.map + (next_y * first_map.width) + next_x)) != '#')
+            if (next_y > 0 && next_y < current_map.height - 1 && (*(current_map.map + (next_y * current_map.width) + next_x)) != '#')
             {
                 y1 = next_y;
-
             }
-            if((*(first_map.map + (next_y * first_map.width) + next_x)) != '#')
+            if ((*(current_map.map + (next_y * current_map.width) + next_x)) != '#')
             {
                 count++;
             }
         }
-        usleep(50 * 1000);
+        usleep(50 * 500);
+    } while (1);
+}
+int main(void)
+{
+    setlocale(LC_ALL, "ko_KR.UTF-8");
+    initscr();
+    start_color();
+    init_pair(WALL_PAIR, COLOR_YELLOW, COLOR_RED);
+    init_pair(GOAL_PAIR, COLOR_MAGENTA, COLOR_GREEN);
+    init_pair(PLAYER_PAIR, COLOR_BLACK, COLOR_WHITE);
+    init_pair(EMPTY_PAIR, COLOR_WHITE, COLOR_BLACK);
+
+    struct Map map1;
+    struct Map map2;
+    struct Map map3;
+    struct Map map4;
+    struct Map map5;
+    struct Map map6;
+
+    loadMap(&map1, "map1.txt");
+    loadMap(&map2, "map2.txt");
+    loadMap(&map3, "map3.txt");
+    loadMap(&map4, "map4.txt");
+    loadMap(&map5, "map5.txt");
+    loadMap(&map6, "map6.txt");
+
+    int select;
+
+    do
+    {
+        char input = 0;
+        map_menu();
+
+        while (kbhit())
+        {
+            input = getchar();
+        }
+        if (input == '1')
+        {
+            clear();
+            start_game(map1);
+        }
+        else if (input == '2')
+        {
+            clear();
+            start_game(map2);
+        }
+        else if (input == '3')
+        {
+            clear();
+            start_game(map3);
+        }
+        else if (input == '4')
+        {
+            clear();
+            start_game(map4);
+        }
+        else if (input == '5')
+        {
+            clear();
+            start_game(map5);
+        }
+        else if (input == '6')
+        {
+            clear();
+            start_game(map6);
+        }
+        else{
+            map_menu();
+        }
+
     } while (1);
 
     endwin();
