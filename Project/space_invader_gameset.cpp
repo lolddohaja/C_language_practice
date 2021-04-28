8170c5ab556aa87fe7329db35b7f0b3e0f573c7e
lolddohaja <sksmsrlqja05@gmail.com>
2021-04-28 14:31:41 +0900

    project

--- Project/space_invader_gameset.cpp
+++ Project/space_invader_gameset.cpp
@@ -51,25 +51,25 @@ void map_menu()
         }
         if (n)
         {
-            mvprintw(0,  0, " = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ");
-            mvprintw(1,  0, "=                                                               =");
-            mvprintw(2,  0, "                          Select  Map                            ");
-            mvprintw(3,  0, "=                          1. level1                            =");
-            mvprintw(4,  0, "                           2. level2                             ");
-            mvprintw(5,  0, "=                          3. level3                            =");
-            mvprintw(6,  0, "                           4. level4                             ");
-            mvprintw(7,  0, "=                          5. level5                            =");
-            mvprintw(8,  0, "                           q.  exit                              ");
-            mvprintw(9,  0, "=                                                               =");
-            mvprintw(9,  0, "                                                                 ");
-            mvprintw(10, 0, "=                             key                               =");
-            mvprintw(11, 0, "                            ←   : a                             ");
-            mvprintw(12, 0, "=                           ↑   : w                            =");
-            mvprintw(13, 0, "                            →   : d                             ");
-            mvprintw(14, 0, "=                           ↓   : s                            =");
-            mvprintw(15, 0, "                      back to the menu: q                        ");
-            mvprintw(16, 0, "=                                                               =");
-            mvprintw(17, 0, " = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ");
+            mvprintw(0,  0, " = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+            mvprintw(1,  0, "=                                                                 ");
+            mvprintw(2,  0, "             ####    ######    ####    #####    ######           =");
+            mvprintw(3,  0, "=           ######   # ## #   ######   ######   # ## #            ");
+            mvprintw(4,  0, "            ###        ##     ##  ##   ##  ##     ##             =");
+            mvprintw(5,  0, "=            ####      ##     ##  ##   ##  ##     ##              ");
+            mvprintw(6,  0, "               ###     ##     ##  ##   ## ##      ##             =");
+            mvprintw(7,  0, "=           ##  ##     ##     ######   #####      ##              ");
+            mvprintw(8,  0, "            ######     ##     ##  ##   ## ###     ##             =");
+            mvprintw(9,  0, "=            ####      ##     ##  ##   ##  ##     ##              ");
+            mvprintw(10, 0, "                                                                 =");
+            mvprintw(11, 0, "=                               key                               ");
+            mvprintw(12, 0, "                            LEFT  : 'a'                          =");
+            mvprintw(13, 0, "=                           RIGHT : 'd'                           ");
+            mvprintw(14, 0, "                            START : 's'                          =");
+            mvprintw(15, 0, "=                                                                 ");
+            mvprintw(16, 0, "                        back to the menu: 'q'                    =");
+            mvprintw(17, 0, "=                                                                 ");
+            mvprintw(18, 0, " = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
             usleep(150000);
             refresh();
             n = !n;
@@ -77,29 +77,28 @@ void map_menu()
         else if (!n)
         {
             refresh();
-            mvprintw(0,  0, "= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
-            mvprintw(1,  0, "                                                                 ");
-            mvprintw(2,  0, "=                         Select  Map                           =");
-            mvprintw(3,  0, "                           1. level1                             ");
-            mvprintw(4,  0, "=                          2. level2                            =");
-            mvprintw(5,  0, "                           3. level3                             ");
-            mvprintw(6,  0, "=                          4. level4                            =");
-            mvprintw(7,  0, "                           5. level5                             ");
-            mvprintw(8,  0, "=                          q.  exit                             =");
-            mvprintw(9,  0, "                                                                 ");
-            mvprintw(9,  0, "=                                                               =");
-            mvprintw(10, 0, "                              key                                ");
-            mvprintw(11, 0, "=                           ←   : a                            =");
-            mvprintw(12, 0, "                            ↑   : w                             ");
-            mvprintw(13, 0, "=                           →   : d                            =");
-            mvprintw(14, 0, "                            ↓   : s                             ");
-            mvprintw(15, 0, "=                     back to the menu: q                       =");
-            mvprintw(16, 0, "                                                                 ");
-            mvprintw(17, 0, "= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+            mvprintw(0,  0, "= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ");
+            mvprintw(1,  0, "                                                                 =");
+            mvprintw(2,  0, "=            ####    ######    ####    #####    ######            ");
+            mvprintw(3,  0, "            ######   # ## #   ######   ######   # ## #           =");
+            mvprintw(4,  0, "=           ###        ##     ##  ##   ##  ##     ##              ");
+            mvprintw(5,  0, "             ####      ##     ##  ##   ##  ##     ##             =");
+            mvprintw(6,  0, "=              ###     ##     ##  ##   ## ##      ##              ");
+            mvprintw(7,  0, "            ##  ##     ##     ######   #####      ##             =");
+            mvprintw(8,  0, "=           ######     ##     ##  ##   ## ###     ##              ");
+            mvprintw(9,  0, "             ####      ##     ##  ##   ##  ##     ##             =");
+            mvprintw(10, 0, "=                                                                 ");
+            mvprintw(11, 0, "                              key                                =");
+            mvprintw(12, 0, "=                           LEFT  : 'a'                           ");
+            mvprintw(13, 0, "                            RIGHT : 'd'                          =");
+            mvprintw(14, 0, "=                           START : 's'                           ");
+            mvprintw(15, 0, "                                                                 =");
+            mvprintw(16, 0, "=                     back to the menu: 'q'                       ");
+            mvprintw(17, 0, "                                                                 =");
+            mvprintw(18, 0, "= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ");
             usleep(150000);
             refresh();
             n = !n;
         }
     }
 }
-
